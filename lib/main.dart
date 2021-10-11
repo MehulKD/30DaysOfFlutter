279d70e0be69a6b4a8266aa727e2bc63592260ad
Mehul Kadam <developer.mehul@gmail.com>
2021-10-11 11:28:35 +0530

    Day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Material(
-      child: Center(
-        child: Container(child: Text("Welcome to 30 days opf flutter")),
-      ),
-    ));
+    return MaterialApp(home: HomePage());
   }
 }
